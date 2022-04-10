766ba86da83a2661499da860efd4f87d9c79cc62
Mahdi Najafi <najafi.mahdi10@gmail.com>
2022-04-10 09:29:02 +0430

    remove description

--- cmd.go
+++ cmd.go
@@ -7,15 +7,15 @@ import (
 )
 
 var (
-	app                    = kingpin.New("chat", "A command-line chat application.")
-	selectedKubeConfigPath = app.Flag("kubeconfig", "kube config path").Default("~/.kube/config").String()
+	app                    = kingpin.New("workspace-tunnel", "")
+	selectedKubeConfigPath = app.Flag("kubeconfig", "Kube config path").Default("~/.kube/config").String()
 
-	portforwardCMD          = app.Command("port-forward", "open port for your workspace")
-	selectedWorkspace       = portforwardCMD.Arg("name", "Your workspace name").Required().String()
-	selectedWorkspaceConfig = portforwardCMD.Arg("workspace", "workspace yaml config").Required().String()
+	portforwardCMD          = app.Command("port-forward", "")
+	selectedWorkspace       = portforwardCMD.Arg("name", "Workspace name").Required().String()
+	selectedWorkspaceConfig = portforwardCMD.Arg("config", "Workspace config file").Required().String()
 
-	getCMD   = app.Command("get", "get resource list")
-	resource = getCMD.Arg("resource", "your resource.").Required().String()
+	getCMD   = app.Command("get", "")
+	resource = getCMD.Arg("resource", "").Required().String()
 )
 
 func init() {
